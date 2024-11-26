3038a841d6a800236e47f6afde6d43a04039e08e
swintegrator <swintegrator@mediatek.com>
2024-11-26 18:18:52 +0800

    [ALPS08631203] Revert "ANDROID: GKI: Reduce vendor data in task_struct"
    [Description]
    This reverts commit 5af1e8b6659893b96af4fc63400e0ebad6476557.
    This is a workaround for build error:
    kernel_device_modules-6.12/drivers/misc/mediatek/sched/eas/sched_main.c:1000:2: error: call to '__compiletime_assert_1032' declared with 'error' attribute: BUILD_BUG_ON failed: sizeof(struct mtk_task) > (sizeof(u64) * ARRAY_SIZE(((struct task_struct *)0)->android_vendor_data1))
     1000 |         MTK_VENDOR_DATA_SIZE_TEST(struct mtk_task, struct task_struct);
          |         ^
    
    [Test]
    boot pass
    
    Signed-off-by: swintegrator <swintegrator@mediatek.com>
    Signed-off-by: Clive Lin <clive.lin@mediatek.com>
    CR-Id: ALPS08631203
    Feature: Kernel SI Operation
    Change-Id: If4e922d553d7e97956ae8cb671bec7f7a9d2f1ef

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1568,7 +1568,7 @@ struct task_struct {
 	struct callback_head		mce_kill_me;
 	int				mce_count;
 #endif
-	ANDROID_VENDOR_DATA_ARRAY(1, 6);
+	ANDROID_VENDOR_DATA_ARRAY(1, 64);
 	ANDROID_OEM_DATA_ARRAY(1, 6);
 
 #ifdef CONFIG_KRETPROBES
